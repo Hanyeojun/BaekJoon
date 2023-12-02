237cebc73c5d34b041131914c4f33322f3281270
Hanyeojun <110558148+Hanyeojun@users.noreply.github.com>
2023-12-03 01:07:42 +0900

    baekjoon solved

---
+++ 4714.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double x;
+    while (cin >> x) {
+        if (x == -1.0) break;
+        cout << fixed;
+        cout.precision(2);
+        cout << "Objects weighing " << x << " on Earth will weigh " << x * 0.167 << " on the moon.\n";
+    }
+}
